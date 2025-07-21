fcf30433e59e3a4256326203598b1cc517542c4e
Gokullv <gokul1455ai@gmail.com>
2025-07-21 14:34:22 +0530

    Create Employee.java

---
+++ Day1/Day 1/Employee.java
@@ -0,0 +1,22 @@
+// Java Program to show Emoployee details
+import java.util.Scanner;
+public class Employee {
+    String name;
+    double salary;
+    void displayInfo() {
+        System.out.println("Name: "+name);
+        System.out.println("Salary: "+salary);
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      Employee e1 = new Employee();
+      System.out.println("Name:");
+      e1.name = sc.nextLine();
+      System.out.println("Salary:");
+      e1.salary = sc.nextDouble();
+      e1.displayInfo();  
+    }
+}
+// Output:
+// Name: Mohan
+// Salary: 25000.50
